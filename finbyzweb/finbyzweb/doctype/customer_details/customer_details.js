5793fd60e6e0381f97447be14365b7f1f0f09660
FinByz <info@finbyz.com>
2018-12-11 10:09:36 +0000

    Created doctype for listing customers

---
+++ finbyzweb/finbyzweb/doctype/customer_details/customer_details.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2018, Finbyz Tech Pvt Ltd and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Customer Details', {
+	refresh: function(frm) {
+		frm.disable_save()
+		frm.set_read_only(true)
+	}
+});
