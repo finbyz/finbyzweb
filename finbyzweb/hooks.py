8526e8345dcb213cda5c10abc755630b942b50c9
FinByz <info@finbyz.com>
2018-12-12 13:42:18 +0000

    Change Footer File

--- finbyzweb/hooks.py
+++ finbyzweb/hooks.py
@@ -28,6 +28,7 @@ web_include_js = [
 	"https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.6/ScrollMagic.min.js",
 	"https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.6/plugins/animation.gsap.min.js",
 	"https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.6/plugins/debug.addIndicators.min.js",
+	"https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.3/plugins/ScrollToPlugin.min.js",
 	"https://cdnjs.cloudflare.com/ajax/libs/scrollify/1.0.19/jquery.scrollify.min.js",
 	"js/splittext.min.js",
 	"js/finbyz.js",
