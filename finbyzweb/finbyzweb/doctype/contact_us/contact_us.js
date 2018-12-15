1c0dc1362de0226b2432f214281a5968804aefeb
FinByz <info@finbyz.com>
2018-12-15 13:27:34 +0000

    Modify Footer,web_form.html,about.html

---
+++ finbyzweb/finbyzweb/doctype/contact_us/contact_us.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, Finbyz Tech Pvt Ltd and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Contact Us', {
+	refresh: function(frm) {
+
+	}
+});
