6054b68e4a28fc312f53e5523ce2370c70d63bd8
FinByz Tech Pvt. Ltd <info@finbyz.com>
2019-08-10 18:38:13 +0530

    Update finbyz.js

--- finbyzweb/public/js/finbyz.js
+++ finbyzweb/public/js/finbyz.js
@@ -20,7 +20,7 @@ $(".ind-module .nav-pills .nav-link").click(
 ) 
 
 $(document).ready(function () {
-	$("div[data-doctype='Web Form'] .page-header-actions-block button").addClass('finbyz-button').removeClass('btn btn-primary');
+	$(".web-form-actions button").addClass('finbyz-button').removeClass('btn btn-primary btn-sm');
 	$(".page-card .page-card-body a").addClass('finbyz-button').removeClass('btn btn-primary');
 	/* phase js */
 	$('.tabanchor').on('click', function (e) {
