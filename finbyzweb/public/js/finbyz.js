68ca5f226317acc47756bbfc3de07d310f79403b
FinByz Tech Pvt. Ltd <info@finbyz.com>
2019-08-10 12:35:01 +0530

    remove removeClass from web-form button

--- finbyzweb/public/js/finbyz.js
+++ finbyzweb/public/js/finbyz.js
@@ -20,7 +20,7 @@ $(".ind-module .nav-pills .nav-link").click(
 ) 
 
 $(document).ready(function () {
-	$("div[data-doctype='Web Form'] .page-header-actions-block button").addClass('finbyz-button').removeClass("btn btn-primary");
+	$("div[data-doctype='Web Form'] .page-header-actions-block button").addClass('finbyz-button');
 	
 	/* phase js */
 	$('.tabanchor').on('click', function (e) {
