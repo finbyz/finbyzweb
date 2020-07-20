c6d8cdfe2d246966266b516ef4e5f6cd79d3fc84
finbyz <info@finbyz.com>
2020-07-20 12:25:36 +0000

    related links and gallery doctype creation

---
+++ finbyzweb/finbyzweb/doctype/gallery_links/gallery_links.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, Finbyz Tech Pvt Ltd and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Gallery Links', {
+	// refresh: function(frm) {
+
+	// }
+});
