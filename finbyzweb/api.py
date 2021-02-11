5cf0b8a01ac3b555538ed265e42f6ba237ae20fd
finbyz <info@finbyz.com>
2021-02-11 04:41:20 +0000

    contact page changes

--- finbyzweb/api.py
+++ finbyzweb/api.py
@@ -23,6 +23,19 @@ def customer_before_save(self, method):
 	frappe.db.commit()
 
 @frappe.whitelist(allow_guest=True)
+def set_form_contact_data(lead_name, company_name, mobile_no, title, email,notes):
+	data = frappe.new_doc("Lead")
+	data.lead_name = lead_name
+	data.company_name = company_name
+	data.mobile_no = mobile_no
+	data.source = 'Website'
+	data.notes = title
+	data.email_id = email
+	data.notes = notes
+	data.save(ignore_permissions=True)
+	
+	frappe.db.commit()
+	
 def set_form_data(lead_name, company_name, mobile_no, title, email):
 	data = frappe.new_doc("Lead")
 	data.lead_name = lead_name
