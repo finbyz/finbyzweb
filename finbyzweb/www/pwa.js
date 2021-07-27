922bae3966e9b22639b513c4fcbe9aee1b8e6d6d
finbyz <info@finbyz.com>
2021-07-27 12:42:10 +0530

    PWA changes for first time

---
+++ finbyzweb/www/pwa.js
@@ -0,0 +1,184 @@
+function showPrompt(buttonText, messageText, f) {
+    const $btn = $(`<button class="pwa-next-action"><span>${__(buttonText)}</span></button>`);
+    const next_action_container = $(`<div class="pwa-next-action-container"></div>`);
+    $btn.click(() => f());
+    next_action_container.append($btn);
+    showPwaAlert({
+        message: messageText,
+        body: next_action_container,
+        indicator: 'green',
+    });
+    $('.btn-pwa-install').click(() => f()).show();
+}
+
+function showPwaAlert(message, seconds = 7) {
+    if (typeof message === 'string') {
+        message = {
+            message: message
+        };
+    }
+    if (!$('#pwa-dialog-container').length) {
+        $('<div id="pwa-dialog-container"><div id="pwa-alert-container"></div></div>').appendTo('body');
+    }
+
+    const div = $(`
+		<div class="pwa-alert pwa-desk-alert">
+			<div class="pwa-alert-message"></div>
+			<div class="pwa-alert-body" style="display: none"></div>
+			<a class="pwa-close">&times;</a>
+		</div>`);
+
+    let pwaMessage = div.find('.pwa-alert-message');
+    pwaMessage.append(message.message);
+
+    if (message.indicator) {
+        pwaMessage.addClass('indicator ' + message.indicator);
+    }
+
+    if (message.body) {
+        div.find('.pwa-alert-body').show().html(message.body);
+    }
+
+    div.hide().appendTo("#pwa-alert-container").show()
+        .css('transform', 'translateX(0)');
+
+    div.find('.pwa-close, button').click(function () {
+        div.remove();
+        return false;
+    });
+
+    div.delay(seconds * 1000).fadeOut(300);
+    return div;
+}
+
+if ('serviceWorker' in navigator) {
+    if ('{{ vapid_public_key }}') {
+        console.log('[PWA] Set VAPID key for push notifications');
+        window.vapidPublicKey = new Uint8Array('{{ vapid_public_key }}');
+    }
+
+    window.addEventListener('load', function () {
+        if (navigator.serviceWorker.controller) {
+            console.log('[PWA] ServiceWorker found, no need to register');
+        } else {
+            navigator.serviceWorker.register('/sw.js').then(
+                function (registration) {
+                    // Registration was successful
+                    console.log('[PWA] ServiceWorker registration successful with scope: ' + registration.scope);
+
+                    if (window.vapidPublicKey) {
+                        console.log('[PWA] Subscribing to push notifications');
+
+                        registration.pushManager.subscribe({
+                            userVisibleOnly: true,
+                            applicationServerKey: window.vapidPublicKey
+                        }).then(
+                            function (pushSubscription) {
+                                console.log(pushSubscription.subscriptionId);
+                                console.log(pushSubscription.endpoint);
+                                // The push subscription details needed by the application
+                                // server are now available, and can be sent to it using,
+                                // for example, an XMLHttpRequest.
+                            }, function (error) {
+                                // During development it often helps to log errors to the
+                                // console. In a production environment it might make sense to
+                                // also report information about errors back to the
+                                // application server.
+                                console.log(error);
+                            }
+                        );
+                    }
+                }, function (err) {
+                    // registration failed :(
+                    console.log('[PWA] ServiceWorker registration failed: ', err);
+                });
+        }
+    });
+
+    var installPromptEvent = null;
+    window.addEventListener('beforeinstallprompt', function (e) {
+        e.preventDefault();
+        installPromptEvent = e;
+        console.log('[PWA] Application could be installed');
+        showAddToHomeScreen();
+    });
+
+    function addToHomeScreen(event) {
+        // Show the prompt
+        hideAddToHomeScreen();
+
+        if (installPromptEvent === null) {
+            console.log('[PWA] No A2HS event to trigger for this device');
+        } else {
+            installPromptEvent.prompt();
+
+            // Wait for the user to respond to the prompt
+            installPromptEvent.userChoice
+                .then(function (choiceResult) {
+                    if (choiceResult.outcome === 'accepted') {
+                        console.log('[PWA] User accepted the A2HS prompt');
+                    } else {
+                        console.log('[PWA] User dismissed the A2HS prompt');
+                    }
+                    installPromptEvent = null;
+                });
+        }
+    }
+
+    function showAddToHomeScreen(event) {
+        if (installPromptEvent === null) {
+            console.log('[PWA] No A2HS event stored for this device');
+        } else {
+            if (window.location.pathname === '/install') {
+                // show to user prompt with PWA installation
+                showPrompt(('{{ _("Install") }}'), '{{ _("Do you want to install PWA?") }}', addToHomeScreen);
+            } else {
+                // show to user prompt with Install Page redirection
+                showPrompt('{{ _("Go to Install Page") }}', '{{ _("This device supports PWA") }}', function () {
+                        window.location.href = "/install";
+                    });
+            }
+        }
+    }
+
+    function hideAddToHomeScreen(event) {
+        $('#pwa-alert-container').hide();
+        $('.btn-pwa-install').hide();
+    }
+
+    window.addEventListener('appinstalled', hideAddToHomeScreen);
+
+    // Push notifications
+    function pushNotification(message) {
+        navigator.serviceWorker.ready
+            .then(function (serviceWorkerRegistration) {
+                serviceWorkerRegistration.pushManager.getSubscription()
+                    .then(function (subscription) {
+                        $.post('/push', {
+                            subscription: subscription.toJSON(),
+                            message: message
+                        });
+                    });
+            });
+    }
+
+    // Request to allow push notifications
+    if (window.vapidPublicKey) {
+        if (!("Notification" in window)) {
+            console.error("[PWA] This browser does not support desktop notification");
+        } else if (Notification.permission === "granted") {
+            console.log("[PWA] Permission to receive notifications has been granted");
+        } else if (Notification.permission !== 'denied') {
+            Notification.requestPermission(function (permission) {
+                // If the user accepts, let's create a notification
+                if (permission === "granted") {
+                    console.log("[PWA] Permission to receive notifications has been granted");
+                }
+            });
+        }
+    }
+} else if (window.location.protocol !== 'https:') {
+    console.warn('[PWA] Requires secure HTTPS connection');
+} else {
+    console.warn('[PWA] No Service Worker support on your device');
+}
---
+++ finbyzweb/www/sw.js
@@ -0,0 +1,140 @@
+// Service Worker
+var CACHE_NAME = 'frappe-cache-v1';
+
+var urlsToCache = [
+    '/',
+    'https://cdn.jsdelivr.net/combine/gh/finbyz/finbyzweb/finbyzweb/public/js/manifest.json',
+    '{{ (favicon or "/assets/frappe/images/favicon.png") | abs_url }}',
+    // CSS
+    'https://cdn.jsdelivr.net/combine/npm/bootstrap-css-only@4.1.0/css/bootstrap.min.css',
+    {%- for link in web_include_css %}
+    '{{ link|abs_url }}',
+    {%- endfor %}
+    // JS
+    'https://cdn.jsdelivr.net/combine/gh/finbyz/finbyzweb/finbyzweb/public/js/frappe-web.min.js,npm/bootstrap@4.1.0/dist/js/bootstrap.min.js',
+    'https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js',
+	{%- for link in web_include_js %}
+    '{{ link|abs_url }}',
+    {%- endfor %}
+];
+
+// Install stage sets up the index page (home page) in the cache and opens a new cache
+this.addEventListener('install', function (event) {
+    console.log('[SW] Install Event processing');
+
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('[SW] Installing cache ' + CACHE_NAME);
+                return cache.addAll(urlsToCache)
+                    .catch(function (err) {
+                        console.log('[SW] Cache install Failed: ' + err);
+                    });
+            }).catch(function (err) {
+                console.error(event.request.url, err);
+                console.log('[SW] Install Failed: ' + err);
+            })
+    );
+});
+
+// If any fetch fails, it will look for the request in the cache and serve it from there first
+this.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                if (!response) {
+                    return handleNoCacheMatch(event);
+                }
+                // Update cache record in the background
+                fetchFromNetworkAndCache(event);
+                // Reply with stale data
+                return response
+            })
+    );
+});
+
+// When activating the Service Worker, clear older caches
+this.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (keys) {
+            return Promise.all(keys.map(function (key) {
+                clearCache(key);
+            }));
+        }));
+});
+
+// Respond to 'push' events and trigger notifications on the registration
+this.addEventListener('push', function (event) {
+    let title = (event.data && event.data.text());
+    let tag = "push-frappe-notification";
+    let icon = '{{ (favicon or "/assets/frappe/images/favicon.png") | abs_url }}';
+
+    event.waitUntil(
+        self.registration.showNotification(title, { icon, tag })
+    );
+});
+
+
+function fetchFromNetworkAndCache(event) {
+    if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') {
+        return;
+    }
+
+    return fetch(event.request).then(function (res) {
+        // foreign requests may be res.type === 'opaque' and missing a url
+        if (!res.url) {
+            return res;
+        }
+        // Only cache GET requests
+        if (event.request.method !== 'GET') {
+            return res;
+        }
+        // regardless, we don't want to cache other origin's assets
+        if (new URL(res.url).origin !== location.origin) {
+            return res;
+        }
+
+        // If request was success, add or update it in the cache
+        updateCache(event.request, res.clone());
+        // TODO: figure out if the content is new and therefore the page needs a reload.
+
+        return res;
+    }).catch(function (err) {
+        console.error(event.request.url, err);
+        console.log('[SW] Network request Failed. Serving content from cache: ' + err);
+        return fromCache(event.request);
+    });
+}
+
+function handleNoCacheMatch(event) {
+    return fetchFromNetworkAndCache(event);
+}
+
+function fromCache(request) {
+    // Check to see if you have it in the cache
+    // Return response
+    // If not in the cache, then return error page
+    return caches.open(CACHE_NAME).then(function (cache) {
+        return cache.match(request).then(function (matching) {
+            if (!matching || matching.status === 404) {
+                return Promise.reject("no-match");
+            }
+
+            return matching;
+        });
+    });
+}
+
+function updateCache(request, response) {
+    return caches.open(CACHE_NAME).then(function (cache) {
+        return cache.put(request, response);
+    });
+}
+
+function clearCache(key) {
+    if (key !== CACHE_NAME) {
+        console.log('[SW] Cleaning cache ' + key);
+        return caches.delete(key);
+    }
+}
