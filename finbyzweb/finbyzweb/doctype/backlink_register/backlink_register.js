138c25b234e606cfc4ba0d74f76e7c1e6629ed4d
Finbyz <info@finbyz.tech>
2023-10-06 11:36:30 +0530

    automatic fetch value of submitted url and apply changes in filter i.e filter on the basis of page_for_type and for_page

--- finbyzweb/finbyzweb/doctype/backlink_register/backlink_register.js
+++ finbyzweb/finbyzweb/doctype/backlink_register/backlink_register.js
@@ -2,7 +2,17 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Backlink Register', {
-	// refresh: function(frm) {
-
-	// }
+	for_page:function(frm)
+	{
+		var selected_doctype=frm.doc.for_page_type;
+		var selected_entry=frm.doc.for_page;
+		frappe.db.get_value(selected_doctype,selected_entry , "route",function(r)
+		{
+			if(r.route)
+			{
+				frm.set_value("submitted_url",`https://finbyz.tech/${r.route}`)
+			}
+		})
+		
+	}
 });
