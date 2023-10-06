0a6a415d34e1113cf98b475779862ed6931ad217
Finbyz <info@finbyz.tech>
2023-10-06 09:42:42 +0530

    add new doctype backlink_register

---
+++ finbyzweb/finbyzweb/doctype/backlink_register/backlink_register.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Finbyz Tech Pvt Ltd and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Backlink Register', {
+	// refresh: function(frm) {
+
+	// }
+});
