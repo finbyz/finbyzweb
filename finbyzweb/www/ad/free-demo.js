1b8af2a6985db1c0cd308e6313e852700fa74dcb
FinByz <info@finbyz.tech>
2024-03-07 14:02:47 +0530

    Ad webpage added

---
+++ finbyzweb/www/ad/free-demo.js
@@ -0,0 +1,30 @@
+async function lead_creation() {
+    frappe.msgprint("sent");
+    frappe.call({
+        method: "finbyzweb.api.set_form_data",
+        args: {
+            'lead_name': $('#lead_name').val(),
+            'company_name': $('#company_name').val(),
+            'mobile_no': $('#mobile_no').val(),
+            'title': document.title + '</br>' + window.location.href,
+            'email': $('#email').val()
+        },
+        callback: function (r) {
+            $('#lead_name').val('');
+            $('#company_name').val('');
+            $('#mobile_no').val('');
+            $('#email').val('');
+        }
+    });
+};
+
+var form = $('#inquiry-ad'),
+    submit = form.find('[name="submit"]');
+
+form.on('submit', async function (e) {
+    await lead_creation();
+    // let label = $('.inquiry-label');
+    // label.removeClass('show');
+    e.preventDefault();
+    window.location.href = "/thank-you-for-inquiry";
+});
