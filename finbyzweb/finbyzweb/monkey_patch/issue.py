9b1924c3b018cfebcdad439a859a2a6bc1917568
finbyz <info@finbyz.tech>
2024-08-08 17:59:36 +0530

    changes for ticket shown on website

--- finbyzweb/finbyzweb/monkey_patch/issue.py
+++ finbyzweb/finbyzweb/monkey_patch/issue.py
@@ -5,25 +5,28 @@ from frappe.utils.user import is_website_user
 
 def get_issue_list(doctype, txt, filters, limit_start, limit_page_length=20, order_by=None):
 
-	user = frappe.session.user
-	contact = frappe.db.get_value("Contact", {"user": user}, "name")
-	customer = None
+    user = frappe.session.user
+    contact = frappe.db.get_value("Contact", {"user": user}, "name")
+    customer = None
 
-	if contact:
-		contact_doc = frappe.get_doc("Contact", contact)
-		customer = contact_doc.get_link_for("Customer")
+    if contact:
+        contact_doc = frappe.get_doc("Contact", contact)
+        customer = contact_doc.get_link_for("Customer")
 
-	ignore_permissions = False
-	if is_website_user():
-		if not filters:
-			filters = {}
-		
-		customer_list = frappe.db.get_all("Portal User", filters={"user": user}, pluck="parent")
-		filters["customer"] = ['IN', customer_list]
+    ignore_permissions = False
+    if is_website_user():
+        if not filters:
+            filters = {}
+        
+        customer_list = frappe.db.get_all("Portal User", filters={"user": user}, pluck="parent")
+        filters["customer"] = ['IN', customer_list]
 
-		ignore_permissions = True
+        ignore_permissions = True
 
-	return get_list(
-		doctype, txt, filters, limit_start, limit_page_length, ignore_permissions=ignore_permissions
-	)
+    if not order_by:
+        order_by = "modified DESC"
+
+    return get_list(
+        doctype, txt, filters, limit_start, limit_page_length, ignore_permissions=ignore_permissions, order_by=order_by
+    )
 
--- finbyzweb/finbyzweb/web_form/ticket/ticket.js
+++ finbyzweb/finbyzweb/web_form/ticket/ticket.js
@@ -1,3 +1,4 @@
 frappe.ready(function() {
-	// bind events here
+	frappe.web_form.set_df_property("assigned_to", "reqd", 0);
+    frappe.web_form.set_df_property("project", "reqd", 0);
 })
