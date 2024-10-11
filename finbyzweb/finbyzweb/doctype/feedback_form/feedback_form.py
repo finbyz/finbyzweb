709361c2f4a41f1f1d9e16c0aa452eba4a3b99df
finbyz <info@finbyz.tech>
2024-10-11 16:17:36 +0530

    PS changes for feedback form

--- finbyzweb/finbyzweb/doctype/feedback_form/feedback_form.py
+++ finbyzweb/finbyzweb/doctype/feedback_form/feedback_form.py
@@ -1,8 +1,15 @@
 # Copyright (c) 2023, Finbyz Tech Pvt Ltd and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class FeedbackForm(Document):
-	pass
+	def before_validate(self):
+		if self.document == "Issue":
+			issue_name = self.document_name  
+   
+			frappe.db.set_value('Issue', issue_name, {
+				'rating': self.rating,     
+				'feedback': self.feedback  
+			})
